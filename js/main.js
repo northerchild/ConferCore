76ea6270bd5c57d7eb2e94b9e7d6754a4f497b6b
Northerchild <osarus-13@hotmail.com>
2018-03-31 19:14:42 -0500

    Se crean las variables para utilizar en el proyecto

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(function(){
+	"use strict"
+	document.addEventListener("DOMContentLoaded",function(){
+
+	//Datos Usuario	
+	let nombre = document.getElementById('nombre')
+	let apellido = document.getElementById('apellido')
+	let email = document.getElementById('email')
+	//Campos pases
+	let pase_dia = document.getElementById('pase_dia')
+	let pase_dosdia = document.getElementById('pase_dosdia')
+	let pase_completo = document.getElementById('pase_completo')
+	//Botones y divs
+	let calcular = document.getElementById('calcular')
+	let errorDiv = document.getElementById('error')
+	let botonRegistro = document.getElementById('btnRegistro')
+	let resultado = document.getElementById('lista-productos')
+	})//DOM CONTENT LOADED
+})()
